b39899afdf63f90a1e0c92d066bd307fcbc31234
need4spd <need4spd@gmail.com>
2011-11-30 23:35:32 +0900

    modify hotreload and delete wrong src

--- src/hotreload/Reloader.java
+++ src/hotreload/Reloader.java
@@ -5,21 +5,18 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 /**
  * @author need4spd, need4spd@cplanet.co.kr, 2010. 8. 23.
  *
  */
 public class Reloader {
-	private Log logger = LogFactory.getLog(Reloader.class);
 
     URLClassLoader appClassLoader = null;
     File dir = new File("d:/");
 
 
-    public void run() throws Exception {
+    @SuppressWarnings("deprecation")
+	public void run() throws Exception {
         URL url = dir.toURL();
         URL[] urls = new URL[]{url};
 
@@ -28,8 +25,9 @@ public class Reloader {
 
             this.appClassLoader = new URLClassLoader(urls);
             //Thread.currentThread().setContextClassLoader( this.appClassLoader );
-            Class appClass = this.appClassLoader.loadClass("BaseDomain");
-            Method printName = appClass.getMethod("printName", null);
+            @SuppressWarnings("rawtypes")
+			Class appClass = this.appClassLoader.loadClass("BaseDomain");
+           Method printName = appClass.getMethod("printName", null);
 
             Object appInstance = appClass.newInstance();
             System.out.println(appInstance);
